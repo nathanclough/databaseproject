8e2819c3dab6da95e47a491d1d7a98c6b43ad6ba
Nathan <nathan.clough@ttu.edu>
2021-11-17 18:58:38 -0600

    Creaeted master file

--- AddForeignKeys.sql
+++ AddForeignKeys.sql
@@ -6,4 +6,20 @@ use [BSLDB]
 alter table dbo.coach 
 Add TeamName varchar(100) Foreign key References dbo.team(TeamName)
 
+alter table dbo.Player add foreign key (BioId) references dbo.bio(id)
+
+alter table dbo.player add Team varchar(100) foreign key references 
+dbo.team(TeamName)
+
+alter table dbo.game add AwayTeam varchar(100) foreign key 
+references dbo.team(TeamName)
+
+alter table dbo.game add HomeTeam varchar(100) foreign key 
+references dbo.team(TeamName)
+
+alter table dbo.game add RefereeId int foreign key 
+references dbo.referee(RefereeId)
+
+alter table dbo.shot add GameId int foreign key references dbo.game(gameid)
+alter table dbo.shot add PlayerId int foreign key references dbo.player(PlayerId)
 
